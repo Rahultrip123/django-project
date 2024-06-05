22519c9e859a135ebfe12a376d6e01f1c60493d0
Rahultrip123 <145183491+Rahultrip123@users.noreply.github.com>
2024-06-05 12:17:23 +0530

    Update urls.py

--- app/urls.py
+++ app/urls.py
@@ -6,7 +6,7 @@ from django.contrib.auth import views as auth_views
 from .forms import LoginForm, MyPasswordChangeForm, MyPasswordResetForm, MySetPasswordForm
 urlpatterns = [
     # path('', views.home),
-    path('', views.ProductView.as_view(), name="index"),
+    path('index/', views.ProductView.as_view(), name="index"),
     path('search/', views.Search, name="search"),
     # path('product-detail', views.product_detail, name='product-detail'),
     path('product-detail/<int:pk>/', views.ProductDetailView.as_view(), name='product-detail'),
